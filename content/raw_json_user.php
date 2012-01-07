7f3d96451ac884cd23ca1c6ec97269fb80a41557
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-07 23:14:48 +0100

    copy parse test

--- content/raw_json_user.php
+++ content/raw_json_user.php
@@ -43,6 +43,47 @@
 				echo "<pre>";
 				print_r($jsonarray);
 				echo "</pre>";
+				
+				
+				
+				
+				
+				// Parsing test
+				///////////////////////////////////////////////
+				/*echo "<b>JSON Array<br></b>";
+				// arraysize of json file.
+				$arraySize = sizeof($jsonarray);
+				echo "arraySize: $arraySize <br>";
+
+				// loop the array
+				for ($i=0; $i<$arraySize; $i++) {
+
+					$content = $jsonarray[$i];
+					echo "array [$i]";
+					echo "<pre>";
+					print_r($content);
+					echo "</pre>";
+					$contentSize = sizeof($content);
+					echo "contentSize: $contentSize <br>";*/
+
+
+					// loop the array
+					//for ($j=0; $j<$contentSize; $j++) {
+						//$contentB = $content[$j];
+						//echo "content [$j]";
+						//echo "<pre>";
+						//print_r($content_b);
+						//echo "</pre>";
+					//}
+
+
+				    //$res = isset($jsonarray[$i]);
+					//echo "i: $i , $res ,  <br>";
+					
+
+					//if (in_array("login", $content[$i])) {
+					   //echo "Irix enthalten";
+					//}
 			?>
 		</section>
 		
