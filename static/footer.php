3132a7d8a69aca3610029408f6383039d9d9a507
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-08 00:50:08 +0100

    cleal foorer code, content changes

--- static/footer.php
+++ static/footer.php
@@ -1,10 +1,39 @@
+
 <!-- footer.php file - begin -->
 <footer>
-</br><img src="http://www.wrong-entertainment.com/code/fhp_white.png" height="35" alt="fh-potsdam, university of applied sciences"/> 
-<img src="http://www.wrong-entertainment.com/code/wng_slogan_white.png" height="40" hspace=30 alt="wrong-entertainment.com creative studiolab for motion and graphicdesign"/> 
-<img src="http://www.wrong-entertainment.com/code/github_white.png" height="45" alt="github, social coding"/></br>
-<p>Paul Vollmer, <b><a href="http://www.wrong-entertainment.com">wrong-entertainment.com </a></b></br>
-Tim Pulver, <b><a href="http://www.timpulver.de">timpulver.de</a></b></p>
-<p>Get the source code on <b><a href="https://github.com/WrongEntertainment/MagicTrackpadOsc">Github</a></b></p>
+<img src="http://www.wrong-entertainment.com/code/GithubViz/img/footerbg.png">
+<div id="footer_content">
+	<img src="http://www.wrong-entertainment.com/code/fhp_white.png" height="35" alt="fh-potsdam, university of applied sciences"/>  
+	<img src="http://www.wrong-entertainment.com/code/github_white.png" height="45" alt="github, social coding"/></br></br></br>
+	<table>
+		<tr>
+		<td><img src="http://www.wrong-entertainment.com/code/GithubViz/img/agent_s_green.png" alt="wrong-entertainment.com creative studiolab for motion and graphicdesign"></td>
+		<td style="color: #00FF00">all items on this website are copyright by wrong-entertainment and powder.<br/>2012, all rights reserved. lovely made in germany.</td>
+		</tr>
+	</table>
+	<table border = "0" width="700">
+		<tr>
+		<td><b><a href="http://www.wrong-entertainment.com/code/GithubViz/">GithubViz</a></b></td>
+		<td> </td>
+		<td> </td>
+		</tr>
+		<tr>
+		<td>developed by:</td>
+		<td>fork this project at:</td>
+		<td><a href="https://www.incom.org" target="_blank">incom</a></td>
+		</tr>
+		<tr>
+		<td>Tim Pulver, <a href="http://www.timpulver.de">timpulver.de</a></td>
+		<td><a href="https://github.com/Powder/GithubViz" target="_blank">github/GithubViz</a></td>
+		<td><a href="http://www.flickr.com/people/7186222@N03/" target="_blank">flickr</a></td>
+		</tr>
+		<tr>
+		<td>Paul Vollmer, <a href="http://www.wrong-entertainment.com">wrong-entertainment.com </a></td>
+		<td> </td>
+		<td><a href="http://www.vimeo.com/wrong" target="_blank">vimeo</a></td>
+		</tr>
+	</table>
+	<br><br>
+</div>
 </footer>
 <!-- footer - end -->
