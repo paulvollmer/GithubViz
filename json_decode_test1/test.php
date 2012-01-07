5e6c9186773390d4de728f550ef445ac3e99e295
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-07 16:53:51 +0100

    update json_decode_test1

--- json_decode_test1/test.php
+++ json_decode_test1/test.php
@@ -1,12 +1,40 @@
-<?php
 
-include 'vars.php';
-echo "User: $user <br>Repo: $repo <br>";
+<html>
+	<?php
+		include 'vars.php';
+		echo "User: $user <br>";
+		echo "Repo: $repo <br>";
+		echo "url: $apiurl <br>";
 
-//echo "test:___ $jsonfile <br>";
+		// print out the json array.
+		echo "<pre>";
+		print_r($jsonarray);
+		echo "</pre>";
+		
+		//echo "test:___ $jsonfile <br>";
 
-echo "<h1>Array</h1>";
-
-echo "$jsonarray[0]";
-
-?>
+		echo "<b>JSON Array<br></b>";		
+		
+		// arraysize of json file.
+		$arraySize = sizeof($jsonarray);
+		echo "arraySize: $arraySize <br>";
+		
+		// loop the array
+		for ($i=0; $i<$arraySize; $i++) {
+			
+			$content1 = $jsonarray[$i];
+			//echo "### §content1 ";
+			$content1size = sizeof($jsonarray[$i]);
+			echo "content1size: $content1size <br>";
+			
+		    $res = isset($jsonarray[$i]);
+	
+			echo "i: $i , $res ,  <br>";
+			
+			/*if (in_array("login", $jsonarray[$i])) {
+			    echo "Irix enthalten";
+			}*/
+		}
+		
+	?>
+</html>
