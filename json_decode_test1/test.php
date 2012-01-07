e1b932c90cfd736621f150c53ec1e97ccbecf052
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-07 17:17:04 +0100

    update test

--- json_decode_test1/test.php
+++ json_decode_test1/test.php
@@ -2,19 +2,19 @@
 <html>
 	<?php
 		include 'vars.php';
+		
+		// print infos
 		echo "User: $user <br>";
 		echo "Repo: $repo <br>";
 		echo "url: $apiurl <br>";
 
-		// print out the json array.
+		// print out the whole json array.
 		echo "<pre>";
 		print_r($jsonarray);
 		echo "</pre>";
-		
 		//echo "test:___ $jsonfile <br>";
 
-		echo "<b>JSON Array<br></b>";		
-		
+		echo "<b>JSON Array<br></b>";
 		// arraysize of json file.
 		$arraySize = sizeof($jsonarray);
 		echo "arraySize: $arraySize <br>";
@@ -22,16 +22,31 @@
 		// loop the array
 		for ($i=0; $i<$arraySize; $i++) {
 			
-			$content1 = $jsonarray[$i];
-			//echo "### §content1 ";
-			$content1size = sizeof($jsonarray[$i]);
-			echo "content1size: $content1size <br>";
+			$content = $jsonarray[$i];
+			echo "array [$i]";
+			echo "<pre>";
+			print_r($content);
+			echo "</pre>";
+			$contentSize = sizeof($content);
+			echo "contentSize: $contentSize <br>";
 			
-		    $res = isset($jsonarray[$i]);
+			
+			// loop the array
+			/*for ($j=0; $j<$contentSize; $j++) {
+				//$contentB = $content[$j];
+				echo "content [$j]";
+				echo "<pre>";
+				print_r($content_b);
+				echo "</pre>";
+			}*/
+			
+			
+		    /*$res = isset($jsonarray[$i]);
 	
 			echo "i: $i , $res ,  <br>";
+			*/
 			
-			/*if (in_array("login", $jsonarray[$i])) {
+			/*if (in_array("login", $content[$i])) {
 			    echo "Irix enthalten";
 			}*/
 		}
