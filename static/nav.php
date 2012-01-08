a8196e32bd51be1fc613b564374dd06ac57eb471
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-08 01:23:13 +0100

    Added navi separator

--- static/nav.php
+++ static/nav.php
@@ -2,11 +2,18 @@
 <!-- nav.php file - begin -->
 <nav>
 <ul id="navigation">
-<li><a href="about.php">About</a></li>
-<li><a href="discover.php">Discover</a></li>
-<li><a href="raw.php">Raw-, Json-Files</a></li>
-<li><a href="https://github.com/Powder/GithubViz">Github</a></li>
+<li><a href="about.php">About</a> | </li>
+<li><a href="discover.php">Discover</a> | </li>
+<li><a href="raw.php">Raw-, Json-Files</a> | </li>
+<li><a href="https://github.com/Powder/GithubViz">Github</a> | </li>
 <li><a href="contact.php">Contact</a></li>
 </ul>
+<br>
 </nav>
+<!-- Breadcrumb 
+<div id="breadcrumb">
+<p><br>
+Breadcrumb: <a href="http://www.wrong-entertainment.com/">Startseite</a><b> > Index</b>
+</p>
+</div>-->
 <!-- nav - end -->
