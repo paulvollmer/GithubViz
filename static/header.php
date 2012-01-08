63932ec30a5c374495006b7c72529231f52547e0
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-08 01:22:47 +0100

    changes at header subline

--- static/header.php
+++ static/header.php
@@ -2,6 +2,6 @@
 <!-- header.php file - begin -->
 <header>
 <h1><a href="http://powder.github.com/GithubViz/">GithubViz </a></h1>
-<div class="subline">a github visualizing for advanced repository control.</div>
+<div class="subline">a github grapher for advanced repository control.</div>
 </header>
 <!-- header - end -->
