052a042f69c2b4d8f1a5935d40b68ebfda776885
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-08 01:29:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,60 +2,52 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-	<head>
-		<!-- ?php include("head.php"); ?> -->
-		<!-- For this file we use the content from head.php -->
-		<!-- Title and Meta Parameter -->
-		<title>GithubViz</title>
-		<meta name="description" content="GithubViz">
-		<meta name="keywords" content="github, wrong entertainment, wrong-entertainment.com">
-		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-		<meta http-equiv="Content-Language" content="en-us" />
-		<meta name="ROBOTS" content="index,follow,archive" />
-		<meta http-equiv="imagetoolbar" content="false" />
-		<meta name="MSSmartTagsPreventParsing" content="true" />
-		<meta name="author" content="Paul Vollmer, wrong-entertainment.com">
-		<meta name="author" content="Tim Pulver, timpulver.de">
-		<meta name="Rating" content="General" />
-		<meta name="revisit-after" content="7 Days" />
-		<meta name="doc-class" content="Living Document" />
-		<!-- CSS file -->
-		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-	</head>
-	<body>
-		<?php include("static/header.php"); ?>
-		<!-- ?php include("nav.php"); ?> -->
-		<!-- For this file we use the content from nav.php -->
-		<!-- Navigation -->
-		<nav>
-			<ul id="Navigation">
-			    <li><a href="content/about.php">About</a></li>
-			    <li><a href="content/discover.php">Discover</a></li>
-			    <li><a href="content/raw.php">Raw-, Json-Files</a></li>
-				<li><a href="https://github.com/Powder/GithubViz">Github</a></li>
-				<li><a href="content/contact.php">Contact</a></li>
-			</ul>
-		</nav>
-		
-		<!-- CONTENT-->
-		<!-- Introduction -->
-		<!--<section id="intro">
-			<span class="small">sending open sound control</span>
-		</section> -->
-		
-		<!-- Main content area -->  
-		<section>
-			<div id="introduction">
-				<img src="http://placehold.it/750x150">
-				<p>Feel free to replace this paragraph ofs d tsdh es vam a sadre.</p>
-			</div>
-		</section>  
-		
-		<!-- Sidebar 
-		<aside>
-			<p>This is a sidebar.</p>
-		</aside> --> 
-		
-		<?php include("static/footer.php"); ?>
-	</body>
+<!-- ?php include("head.php"); ?> -->
+<!-- For this file we use the content from head.php -->
+<!-- head.php file - begin --> 
+<head>
+<title>GithubViz</title>
+<meta name="description" content="GithubViz">
+<meta name="keywords" content="github, wrong entertainment, wrong-entertainment.com">
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="en-us" />
+<meta name="ROBOTS" content="index,follow,archive" />
+<meta http-equiv="imagetoolbar" content="false" />
+<meta name="MSSmartTagsPreventParsing" content="true" />
+<meta name="author" content="Paul Vollmer, wrong-entertainment.com">
+<meta name="author" content="Tim Pulver, timpulver.de">
+<meta name="Rating" content="General" />
+<meta name="revisit-after" content="7 Days" />
+<meta name="doc-class" content="Living Document" />
+<link rel="stylesheet" type="text/css" href="http://www.wrong-entertainment.com/code/GithubViz/css/stylesheet.css">
+</head>
+<!-- head - end -->
+
+<body>
+<?php include("static/header.php"); ?>
+<!-- ?php include("nav.php"); ?> -->
+<!-- For this file we use the content from nav.php -->
+<!-- nav.php file - begin -->
+<nav>
+<ul id="navigation">
+<li><a href="about.php">About</a> | </li>
+<li><a href="discover.php">Discover</a> | </li>
+<li><a href="raw.php">Raw-, Json-Files</a> | </li>
+<li><a href="https://github.com/Powder/GithubViz">Github</a> | </li>
+<li><a href="contact.php">Contact</a></li>
+</ul>
+<br>
+</nav>
+<!-- nav - end -->
+
+<!-- CONTENT begin -->
+<!-- Main content area -->  
+<section>
+<h1>Discover your Github Network</h1>
+<img src="http://placehold.it/750x150/808080/4D4D4D">
+<p>Feel free to replace this paragraph ofs d tsdh es vam a sadre.</p>
+</section>  
+
+<?php include("static/footer.php"); ?>
+</body>
 </html>
