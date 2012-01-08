755dea897815e5325d57395a763427140b41aa4e
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-08 02:03:53 +0100

    Change raw link name

--- static/nav.php
+++ static/nav.php
@@ -4,7 +4,7 @@
 <ul id="navigation">
 <li><a href="about.php">About</a> | </li>
 <li><a href="discover.php">Discover</a> | </li>
-<li><a href="raw.php">Raw-, Json-Files</a> | </li>
+<li><a href="raw.php">Raw Files</a> | </li>
 <li><a href="https://github.com/Powder/GithubViz">Github</a> | </li>
 <li><a href="contact.php">Contact</a></li>
 </ul>
