1eaa5ad2dc2d7608a573bb96ca6144625f9bd271
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-08 22:44:14 +0100

    Added counter php

--- static/head.php
+++ static/head.php
@@ -13,5 +13,21 @@
 <meta name="Rating" content="General" />
 <meta name="revisit-after" content="7 Days" />
 <meta name="doc-class" content="Living Document" />
+
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+
 <link rel="stylesheet" type="text/css" href="http://www.wrong-entertainment.com/code/GithubViz/css/stylesheet.css">
+<?php
+// counter script
+$file = fopen("../counter.txt","r+");
+$counter = fgets($file, 10);
+if($counter == ""){
+  $counter = 0;
+}
+$counter++;
+//echo $counter;
+rewind($file);
+fwrite($file, $counter);
+fclose($file);
+?>
 <!-- head - end -->
