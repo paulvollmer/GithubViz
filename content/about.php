72ae40259ca23d4303a5446473f056450995fd6e
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-08 15:59:15 +0100

    Rename test.pde

--- content/about.php
+++ content/about.php
@@ -12,25 +12,53 @@ function drawSomeText(id) {
   var text = document.getElementById('inputtext').value;
 
 
-/*
-  Creates an unordered list containing Github user info and an image with the avatar.
-  Will be placed inside a div-element with id "github_userinfo"
-*/
+  /*
+   * Creates an unordered list containing Github user data.
+   * Will be placed inside a div-element with id "github_userinfo"
+   */
   var gitUsername = text;
   var gitBaseUrl = "http://github.com/api/v2/json/";
   var gitUserUrl = "user/show/";
   var gitRepoUrl = "repos/show/";
   var callback = "?callback=?";	
   var gravatarUrl = "http://www.gravatar.com/avatar/";
-	 
   $.getJSON(gitBaseUrl + gitUserUrl + gitUsername + callback,
   function(json, statuts) {			
     var user_data = json.user;
-    //alert(user_data.login);
-    $("#github_userinfo").append("<ul id='github_userinfo-list'></ul>");
-    pjs.drawText(user_data.login, user_data.followers_count, user_data.public_repo_count);
-    $("#github_userinfo-list").append("<li>Public Repos: " + user_data.public_repo_count + "</li>");
-    $('#github_userinfo').prepend('<img id="github_avatar" src="' + gravatarUrl + user_data.gravatar_id + '" />');
+    // binding processing.js function
+    pjs.userdata(user_data.gravatar_id,
+	user_data.company,
+	user_data.name,
+	user_data.created_at,
+	user_data.location,
+	user_data.public_repo_count,
+	user_data.public_gist_count,
+	user_data.blog,
+	user_data.following_count,
+	user_data.id,
+	user_data.type,
+	user_data.permission,
+	user_data.followers_count,
+	user_data.login,
+	user_data.email);
+
+  });
+
+  /**
+   * Generates an unordered list containing all the repositories of a certain user.
+   * The list will be added to the div-element with id "github_repositories".
+   */
+  var username = "Powder";
+  $.getJSON("http://github.com/api/v2/json/repos/show/" + username + "?callback=?",
+  function(json, statuts) {
+    //$("#github_repositories").append("<ul id='repositoriy-list'></ul>");
+	$.each(json.repositories, function(i){
+		//alert(json.repositories.length);
+		// binding processing.js function
+		var type = this['type'];
+		var name = this['name'];
+		pjs.repodata(type);
+    });
   });
 
 
@@ -50,7 +78,7 @@ function drawSomeText(id) {
 <section>
 <h1>Processing.js Test</h1>
 <!-- Processing canvas-->
-<canvas id="sketch1" data-processing-sources="http://www.wrong-entertainment.com/code/GithubViz/js/test.pde"></canvas><br><br>
+<canvas id="sketch1" data-processing-sources="http://www.wrong-entertainment.com/code/GithubViz/js/githubviz.pde"></canvas><br><br>
 <input type="textfield" value="Powder" id="inputtext"/>  
 <button onclick="drawSomeText('sketch1')" id="button4"/>Send</button>
 </section>
