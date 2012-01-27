820718c8af4663031585b958f72563cf4154c82d
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-27 16:49:10 +0100

    added boilerplate template javascript

--- static/footer.php
+++ static/footer.php
@@ -16,7 +16,7 @@
 	
 	<table width="750">
 		<tr>
-		<td><b><a href="GitHubViz">GitHubViz</a></b></td>
+		<td><b><a href="index.php">GitHubViz</a></b></td>
 		<td> </td>
 		<td> </td>
 		<td> </td>
@@ -49,4 +49,30 @@
 	</div>
 </div>
 </footer>
+
+
+<!-- JavaScript at the bottom for fast page loading -->
+<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+  <!-- scripts concatenated and minified via build script 
+  <script defer src="js/plugins.js"></script> -->
+  <!-- end scripts -->
+
+  <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID. mathiasbynens.be/notes/async-analytics-snippet 
+  <script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script> -->
+
+  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+  <!--[if lt IE 7 ]>
+  <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+
 <!-- footer - end -->
