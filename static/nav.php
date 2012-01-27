b301afcb5dfad8c2e1545a437f803fc9e2a197c8
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-27 16:48:46 +0100

    delete breadcrump, changes at subline

--- static/nav.php
+++ static/nav.php
@@ -1,24 +1,17 @@
 
 <!-- nav.php file - begin -->
-<a href="githubviz"><img style="position: absolute; top: 0; right: 0; border: 0; padding: 15px;" src="img/githubviz_logo.png" alt="GitHubViz" /></a>
+<img style="position: absolute; top: 0; right: 0; border: 0; padding: 15px;" src="img/githubviz_logo.png" alt="GitHubViz"/>
 
-<p style="position: absolute; top: 0; right: 0; border: 0; padding-right: 15px; padding-top: 30px;font-size: 9pt; color: #3399FF">a github grapher for advanced repository control</p>
+<p style="position: absolute; top: 0; right: 0; border: 0; padding-right: 15px; padding-top: 30px;font-size: 9pt; color: #3399FF">Visualize your Social Coding Projects</p>
 
 <nav>
-<ul id="navigation">
-<li><a href="about.php">About</a> | </li>
-<li><a href="discover.php">Discover</a> | </li>
-<li><a href="raw.php">Raw Data</a> | </li>
-<li><a href="contact.php">Contact</a></li>
-</ul>
-<br>
+  <ul id="navigation">
+    <li><a href="about.php">About</a> | </li>
+    <li><a href="discover.php">Discover</a> | </li>
+    <li><a href="raw.php">Raw Data</a> | </li>
+    <li><a href="contact.php">Contact</a> </li>
+  </ul>
+  <br/>
 </nav>
-
-<!-- Breadcrumb 
-<div id="breadcrumb">
-<p><br>
-Breadcrumb: <a href="http://www.wrong-entertainment.com/">Startseite</a><b> > Index</b>
-</p>
-</div>-->
 
 <!-- nav - end -->
