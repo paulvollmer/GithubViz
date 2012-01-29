1b10469e282a4d70bd31cc67cf60c03595d62523
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-29 16:31:04 +0100

    php comment out unused code

--- static/footer.php
+++ static/footer.php
@@ -1,56 +1,57 @@
-
-<!-- footer.php file - begin -->
 <footer>
 <img src="img/footerbg.png">
 <div id="footer_content">
-	<table>
-		<colgroup width="150" span="4"></colgroup>
-		<tr align="right" valign="middle">
-		<td><img src="img/fhp_white.png" alt="fh-potsdam, university of applied sciences"/></td>
-		<td><img src="img/github_white.png" alt="github, social coding"/></td>
-		<td><img src="img/powder_white.png" alt="Powder"/></td>
-		<td><img src="img/wrongagent_white.png" alt="wrong-entertainment"/></td>
-		</tr>
-	</table>
-	</br></br></br>
-	
-	<table width="750">
-		<tr>
-		<td><b><a href="index.php">GitHubViz</a></b></td>
-		<td> </td>
-		<td> </td>
-		<td> </td>
-		</tr>
+  <table>
+    <colgroup width="150" span="4"></colgroup>
+    <tr align="right" valign="middle">
+      <td><img src="img/fhp_white.png" alt="fh-potsdam, university of applied sciences"/></td>
+      <td><img src="img/github_white.png" alt="github, social coding"/></td>
+      <td><img src="img/powder_white.png" alt="Powder"/></td>
+      <td><img src="img/wrongagent_white.png" alt="wrong-entertainment"/></td>
+    </tr>
+  </table>
+  </br></br></br>
+
+  <table width="750">
+    <tr>
+	  <td><b><a href="index.php">GitHubViz</a></b></td>
+	  <td> </td>
+	  <td> </td>
+	  <td> </td>
+	</tr>
 		
-		<tr>
-		<td>Developed by</td>
-		<td>Developed at</td>
-		<td>Fork this Project at</td>
-		<td><a href="http://www.github.com/" target="_blank">GitHub</a></td>
-		</tr>
+	<tr>
+	  <td>Developed by</td>
+	  <td>Developed at</td>
+	  <td>Fork this Project at</td>
+	  <td><a href="http://www.github.com/" target="_blank">GitHub</a></td>
+	</tr>
 		
-		<tr>
-		<td>Tim Pulver, <a href="http://www.timpulver.de" target="_blank">timpulver.de</a></td>
-		<td><a href="http://www.fh-potsdam.de" target="_blank">FH Potsdam, University of Applied Sciences</a></td>
-		<td><a href="https://github.com/Powder/GithubViz" target="_blank">github/GithubViz</a></td>
-		<td><a href="http://www.incom.org/" target="_blank">Incom</a></td>
-		</tr>
+	<tr>
+	  <td>Tim Pulver, <a href="http://www.timpulver.de" target="_blank">timpulver.de</a></td>
+	  <td><a href="http://www.fh-potsdam.de" target="_blank">FH Potsdam, University of Applied Sciences</a></td>
+	  <td><a href="https://github.com/Powder/GithubViz" target="_blank">github/GithubViz</a></td>
+	  <td><a href="http://www.incom.org/" target="_blank">Incom</a></td>
+	  </tr>
 		
-		<tr>
-		<td>Paul Vollmer, <a href="http://www.wrong-entertainment.com" target="_blank">wrong-entertainment.com </a></td>
-		<td> </td>
-		<td> </td>
-		<td><a href="http://www.processingjs.com/" target"_blank">processing.js</td>
-		</tr>
-	</table>
-	
-	<div style="color: #3399FF;">
-		<br>Copyleft 2012<br>Lovely made in Germany.<br><br>
-	</div>
+	<tr>
+	  <td>Paul Vollmer, <a href="http://www.wrong-entertainment.com" target="_blank">wrong-entertainment.com </a></td>
+	  <td> </td>
+	  <td> </td>
+	  <td><a href="http://www.processingjs.com/" target"_blank">processing.js</td>
+	</tr>
+  </table>
+
+  <div style="color: #3399FF;">
+    <br>Copyleft 2012<br>Lovely made in Germany.<br><br>
+  </div>
+
 </div>
 </footer>
 
 
+<?php
+/*
 <!-- JavaScript at the bottom for fast page loading -->
 <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
@@ -74,5 +75,6 @@
   <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
   <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
   <![endif]-->
+*/
 
-<!-- footer - end -->
+?>
