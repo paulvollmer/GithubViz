a9ea81678e83d266898204b246e59c8e914fe2ae
Paul Vollmer <wrongentertainment@yahoo.de>
2012-01-29 16:30:41 +0100

    change to echo method

--- static/head.php
+++ static/head.php
@@ -1,47 +1,30 @@
-
-<!-- head.php file - begin -->
-
-<meta charset="utf-8">
-<!-- Use the .htaccess and remove these lines to avoid edge case issues.
-     More info: h5bp.com/b/378 -->
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-<title>GitHubViz</title>
-<meta name="description" content="GitHubViz">
-<meta name="keywords" content="github, github visualisation, visualisation, data, timpulver.de, wrong entertainment, wrong-entertainment.com">
-<meta name="author" content="Paul Vollmer, wrong-entertainment.com">
-<meta name="author" content="Tim Pulver, timpulver.de">
-
-<!-- Mobile viewport optimized: h5bp.com/viewport -->
-<meta name="viewport" content="width=device-width,initial-scale=1">
-
-<!-- Place favicon.ico and apple-touch-icon.png in the root directory:
-     mathiasbynens.be/notes/touch-icons -->
-
-<link rel="stylesheet" href="css/style.css">
-
-<!-- All JavaScript at the bottom, except this Modernizr build incl. Respond.js
-     Respond is a polyfill for min/max-width media queries.
-     Modernizr enables HTML5 elements & feature detects;
-     for optimal performance, create your own custom Modernizr build:
-     www.modernizr.com/download/ -->
-<script src="js/libs/modernizr-2.0.6.min.js"></script>
-
-
-
-<!-- counter script 
 <?php
-/*$file = fopen("../counter.txt","r+");
-$counter = fgets($file, 10);
-if($counter == ""){
-  $counter = 0;
-}
-$counter++;
-//echo $counter;
-rewind($file);
-fwrite($file, $counter);
-fclose($file);*/
-?> -->
 
+echo"<meta charset=\"utf-8\">\n";
 
-<!-- head - end -->
+// Use the .htaccess and remove these lines to avoid edge case issues.
+// More info: h5bp.com/b/378
+echo"<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">
+<title>GitHubViz</title>
+<meta name=\"description\" content=\"GitHubViz\">
+<meta name=\"keywords\" content=\"github, github visualisation, visualisation, data, timpulver.de, wrong entertainment, wrong-entertainment.com\">
+<meta name=\"author\" content=\"Paul Vollmer, wrong-entertainment.com\">
+<meta name=\"author\" content=\"Tim Pulver, timpulver.de\">\n";
+
+// Mobile viewport optimized: h5bp.com/viewport -->
+echo"<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">\n";
+
+// Import css stylesheet
+echo"<link rel=\"stylesheet\" href=\"css/style.css\">\n";
+
+// Import the counter.php file for counting every site load.
+include("static/counter.php");
+
+// All JavaScript at the bottom, except this Modernizr build incl. Respond.js
+// Respond is a polyfill for min/max-width media queries.
+// Modernizr enables HTML5 elements & feature detects;
+// for optimal performance, create your own custom Modernizr build:
+// www.modernizr.com/download/
+echo"<script src=\"js/libs/modernizr-2.0.6.min.js\"></script>";
+
+?>
