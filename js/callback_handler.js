f6c35729f3148af2d8093ce41dfe029fda4ef8fd
Paul Vollmer <wrongentertainment@yahoo.de>
2012-05-08 14:20:39 +0200

    rename drawFollower to userDataReady

--- js/callback_handler.js
+++ js/callback_handler.js
@@ -1,4 +1,3 @@
-
 /*
  * Writes the username and blog Url in the DOM using jQuery
  * The elements will be created inside the element with id "userData" (at the end). 
@@ -9,32 +8,27 @@
  * Look up the names here: https://github.com/judofyr/github-js/blob/master/README.markdown
  */
 var displayUserData = function(userData){
-	// AVATAR -------------------------------------------------------------	
+	// AVATAR -------------------------------------------------------------
 	if(userData.id != null){
 		$("#userData").append('<a href="http://github.com/' + userData.login + '" title="Visit on GitHub" target="_blank"><img id="userId" width="120px" src=" http://www.gravatar.com/avatar/' + userData.gravatar_id + '" /></a>');
 	}
 	
 	// LOGIN --------------------------------------------------------------
 	$("#userData").append('<a href="http://github.com/' + userData.login + '" title="Visit on GitHub"><h1 id="userLogin">' + userData.login + '</h1></a>').log("userData appended!");
-	// BLOG ---------------------------------------------------------------
+	
+    // BLOG ---------------------------------------------------------------
 	if(userData.blog != null){
 		var shortBlog = userData.blog.replace("http://", "").replace("www.", "");
 		$("#userData").append('<a id="userBlog" href="' + userData.blog + '" title="Homepage">' + shortBlog + '</a>').log("blog appended!");
 	}
+    
+    // MAP -----------------------------------------------------------------
 	if(userData.location != null){
 		// On success a map will be drawn
 		getLatLong(userData.location, latLongCallback);
 	}
 	
+    // PJS -----------------------------------------------------------------
 	var p5 = Processing.getInstanceById('gitHubVizCanvas');
-	p5.drawFollower(userData.followers_count);	
+    p5.userDataReady();
 }
-
-/*
- * Not finished yet, just for testing
- */
-/*
-var displayRepoData = funtion(repoData){
-	$("#repoData").append('<p id="repoName (First)">' + userData.repos[0].name + '</p>').log("repoName appended!");	
-}
-*/
